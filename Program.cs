645a79752abecc4b391b07ef7d30969a0283e8ab
Sachin Raj <h.sachin@outlook.com>
2018-12-23 14:28:09 -0800

    Updating code - WIP

--- Program.cs
+++ Program.cs
@@ -9,18 +9,19 @@ using System.Text;
 namespace ConsoleApp2
 {
     #region defineBoards
-    public class BoardsList
-    {
-        public List<Board> board { get; set; }
-        public string TeamName { get; set; }
+    //public class BoardsList
+    //{
+    //    public List<Board> board { get; set; }
+    //    public string TeamName { get; set; }
 
-    }
+    //}
 
-    public class Boards
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-    }
+    //public class boards
+    //{
+    //    public string id { get; set; }
+    //    public string name { get; set; }
+    //    public list<board> board { get; set; }
+    //}
 
     public class Board
     {
@@ -31,7 +32,7 @@ namespace ConsoleApp2
         public string name { get; set; }
         public int revision { get; set; }
 
-        public List<BoardColumn> columns { get; set; }
+       public List<BoardColumn> columns { get; set; }
 
         public List<BoardFields> fields { get; set; }
 
@@ -67,10 +68,16 @@ namespace ConsoleApp2
         public string referenceName { get; set; }
     }
 
-    public class Teams
+    public class Team
     {
         public string name { get; set; }
         public string description { get; set; }
+
+        public List<Board> boards { get; set; }
+
+        public string bugsBehavior { get; set; }
+
+
     }
     #endregion
 
@@ -79,42 +86,47 @@ namespace ConsoleApp2
     {
         private static void Main(string[] args)
         {
-            string personalAccessToken = "glljbia2pjadsd7mjotsrxs3rxiwcl5t2lzxeixdi7msf5x64ukq";// "xfhozufmatsn6c26tbp6ejhqzvtlwcqqk7gh3l4blqgsttntifna";
+            string personalAccessToken = "xfhozufmatsn6c26tbp6ejhqzvtlwcqqk7gh3l4blqgsttntifna";
             string credentials = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalAccessToken)));
             string projectName = "Test3";
+            string acctName = "sachinraj";
             string VersionNumber = "?api-version=5.0-preview.1";
 
             Console.WriteLine("Reading JSON File");
-            BoardsList _boardsList = new BoardsList();
-            List<Teams> _teams = new List<Teams>();
+            List<Board> _boardsList = new List<Board>();
+            List<Team> _teams = new List<Team>();
 
             using (StreamReader r = new StreamReader("..\\..\\Template\\Boards.json"))
             {
                 string json = r.ReadToEnd();
-                _boardsList = JsonConvert.DeserializeObject<BoardsList>(json);
+                _teams = JsonConvert.DeserializeObject<List<Team>>(json);
             }
+            /*
+            
             using (StreamReader r = new StreamReader("..\\..\\Template\\Teams.json"))
             {
                 string json = r.ReadToEnd();
                 _teams = JsonConvert.DeserializeObject<List<Teams>>(json);
             }
+            */
 
             Console.WriteLine("Finished Reading...Provisioning Now");
 
             //looping throw teams and taking one team at a time
             foreach (var team in _teams)
             {
+                _boardsList = team.boards;
                 string TeamasJSON = JsonConvert.SerializeObject(team);
                 using (var client = new HttpClient())
                 {
-                    client.BaseAddress = new Uri("https://dev.azure.com/sachinraj");  //url of your organization
+                    client.BaseAddress = new Uri("https://dev.azure.com/");  //url of your organization
 
                     client.DefaultRequestHeaders.Accept.Clear();
                     client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
                     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
 
                     var jsonContent = new StringContent(TeamasJSON, Encoding.UTF8, "application/json");
-                    var request = client.PostAsync("/_apis/projects/" + projectName + "/teams" + VersionNumber, jsonContent);
+                   /* var request = client.PostAsync(acctName+"/_apis/projects/" + projectName + "/teams" + VersionNumber, jsonContent);
                     var response = request.Result;
 
                     if (response.IsSuccessStatusCode)
@@ -126,20 +138,26 @@ namespace ConsoleApp2
                         Console.WriteLine(String.Format("Error creating team due to {0}", response.StatusCode));
                         return;
                     }
+                    */
                     //Here directly we can't update the board columns, since we can't delete existing incoming and outgoing columns.
                     // We can fetch the existing column IDs for the Team Project and Update the JSON with those IDs to respective incoming and outgoing columns
                     //Then we can update the Board
 
                     //Setting Board Options
-                    foreach (Board _board in _boardsList.board)
+                    foreach (Board _board in _boardsList)
                     {
-                        string boardoptions = JsonConvert.SerializeObject(_board.columns);
+                        string boardoptions = JsonConvert.SerializeObject(_board);
                         var boardAsJson = new StringContent(boardoptions, Encoding.UTF8, "application/json");
-                        var request1 = client.PutAsync("https://dev.azure.com/devaccounts/" + projectName + "/" + team.name + "/_apis/work/boards/" + _board.name + "/columns" + VersionNumber, boardAsJson);
-                        var resp = request1.Result;
+                        var request1 = client.PutAsync(acctName+"/" + projectName + "/" + team.name + "/_apis/work/boards/" + _board.name + VersionNumber, boardAsJson);
+                        var response = request1.Result;
                         if (request1.Result.IsSuccessStatusCode)
                         {
                             Console.WriteLine(String.Format("Sucessfully Created Board {0} for team {1}", _board.name, team.name));
+                        }
+                        else
+                        {
+                            Console.WriteLine(String.Format("Error creating team due to {0}", response.StatusCode));
+                            return;
                         }
                     }
                 }
